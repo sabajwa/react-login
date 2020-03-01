096318b0236f47ba16761e4c1c98ed7c66f23fff
saeed <sabajwa@yahoo.com>
2020-03-01 12:15:54 -0500

    heroku 2

--- server/index.js
+++ server/index.js
@@ -84,5 +84,5 @@ app.get("/api/user/logout", auth, (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("server running at " + { port });
+  console.log("server running at port required ", { port });
 });
